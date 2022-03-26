a39862fc2db47a609bc4b616bdadf87097040f13
Shane McElrath <shane@rangermac.com>
2022-03-26 16:16:49 -0400

    Finised portfolio links

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -5,6 +5,10 @@ function AboutMe() {
     <section id="about-me">
       <h2>About<br/>Me</h2>
       <div>
+        <img
+          src={require('../../assets/Profile/ProfileMe.JPEG')}
+          alt="Shane McElrath"
+        />
         <p>
           My name is Shane, and this website is my digital portfolio.  
           My passion is for software development with experience programming HTML5, CSS3, JavaScript, Node, React, SQL, and MongoDB.  
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,35 +4,47 @@ function Portfolio() {
   return (
     <section id="work">
       <h2>Work</h2>
-      <div class="projects"> 
-        <div class="run-buddy project">
-          <a href="https://eloncoin.herokuapp.com/">
-            {/* hover transition layer */}
-            <div class="color-shift"></div>
-          </a>
-          <div class="project-lable">
-            <h3>ElonCoin</h3>
-            Node / HandleBars
+      <div className="projects"> 
+        <div className="first project">
+          {/* hover transition layer */}
+          <div className="color-shift">
+            <div className="project-lable">
+              <a href="https://eloncoin.herokuapp.com/">
+                <h3>ElonCoin</h3>
+                Node / HandleBars
+              </a>
+            </div>
+            <div className="project-lable github">
+              <a href="https://github.com/ShaneMcElrath/ElonCoin"><h3>Github</h3></a>
+            </div>
           </div>
         </div>
-        <div class="robot-gladiators project">
-          <a href="https://shanemcelrath.github.io/run-buddy/">
-            {/* hover transition layer */}
-            <div class="color-shift"></div>
-          </a>
-          <div class="project-lable">
-            <h3>Run Buddy</h3>
-            HTML / CSS
+        <div className="second project">
+          {/* hover transition layer */}
+          <div className="color-shift">
+            <div className="project-lable">
+              <a href="https://shanemcelrath.github.io/run-buddy/">
+                <h3>Run Buddy</h3>
+                HTML / CSS
+              </a>
+            </div>
+            <div className="project-lable github">
+              <a href="https://github.com/ShaneMcElrath/run-buddy"><h3>Github</h3></a>
+            </div>
           </div>
         </div>
-        <div class="coming-soon project">
-          <a href="https://rebeccadherrera.github.io/shellter/">
-            {/* hover transition layer */}
-            <div class="color-shift"></div>
-          </a>
-          <div class="project-lable">
-            <h3>Shellter</h3>
-            HTML / Javascript / CSS
+        <div className="third project">
+          {/* hover transition layer */}
+          <div className="color-shift">
+            <div className="project-lable">
+              <a href="https://rebeccadherrera.github.io/shellter/">
+                <h3>Shellter</h3>
+                HTML / Javascript / CSS
+              </a>
+            </div>
+            <div className="project-lable github">
+              <a href="https://github.com/rebeccadherrera/shellter"><h3>Github</h3></a>
+            </div>
           </div>
         </div>
       </div>
